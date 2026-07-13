a1402291f241df7d4658c911c52110c32e7d9a20
HaeengIn <haeengin@gmail.com>
2026-07-14 00:30:06 +0900

    Add heuristic function for A* pathfinding

--- src/pathfinder.py
+++ src/pathfinder.py
@@ -4,6 +4,15 @@ from graph import create_graph
 graph = create_graph()
 
 
+def heuristic(current: str, goal: str) -> int:
+    current_floor = graph.nodes[current]["floor"]
+    goal_floor = graph.nodes[goal]["floor"]
+
+    return abs(current_floor - goal_floor)
+
+
 def find_path(start: str, goal: str):
-    # Return opimized path between start and goal
-    return networkx.astar_path(graph, start, goal, weight="weight")
+    path = networkx.astar_path(graph, start, goal, heuristic=heuristic, weight="weight")
+    cost = networkx.path_weight(graph, path, weight="weight")
+
+    return path, cost
