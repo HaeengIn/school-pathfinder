00b35001ef97003b65cd1de83f438f5df86f7b9a
HaeengIn <haeengin@gmail.com>
2026-07-14 00:35:02 +0900

    Add place validation function to check existence in nodes

---
+++ src/validator.py
@@ -0,0 +1,13 @@
+from loader import load_json
+from utils import normalize_place
+
+nodes = load_json("nodes.json")
+
+
+def validate_place(place: str) -> str:
+    place = normalize_place(place)
+
+    if place not in nodes:
+        raise ValueError(f"'{place}'은(는) 존재하지 않는 장소입니다.")
+
+    return place
